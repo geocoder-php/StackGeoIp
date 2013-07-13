8c53ea4e97177b8ea3d404dc8b8c60a8d76535d3
Andy Leon <acleon@acleon.co.uk>
2013-07-13 11:55:02 +0100

    Create mock and stub out calls to provider

--- tests/integration/GeoIpTest.php
+++ tests/integration/GeoIpTest.php
@@ -8,12 +8,28 @@ class GeoIpTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+        $provider = $this->getMockBuilder('\\Geocoder\\Provider\\FreeGeoIpProvider')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $provider->expects($this->any())
+            ->method('getGeocodedData')
+            ->will($this->returnValueMap(array(
+                array('127.0.0.1', array()),
+                array('202.158.214.106', array('countryCode' => 'AU')),
+                array('70.38.0.135', array('countryCode' => 'CA')),
+                array('83.142.228.128', array('countryCode' => 'GB')),
+                array('128.30.2.36', array('countryCode' => 'US')),
+            )))
+        ;
+
         $app = new \Stack\CallableHttpKernel(function(Request $request) {
             return new Response($request->headers->get('X-Country', 'NONE'));
         });
 
         $stack = new \Stack\Builder();
-        $stack->push('\Ducks\Stack\GeoIp');
+        $stack->push('\Ducks\Stack\GeoIp', array('provider' => $provider));
 
         $this->app = $stack->resolve($app);
     }
@@ -45,6 +61,27 @@ class GeoIpTest extends PHPUnit_Framework_TestCase
             array('70.38.0.135', 'CA'),
             array('83.142.228.128', 'GB'),
             array('128.30.2.36', 'US'),
+        );
+    }
+
+    public function getDefaults()
+    {
+        return array(
+            'latitude'      => null,
+            'longitude'     => null,
+            'bounds'        => null,
+            'streetNumber'  => null,
+            'streetName'    => null,
+            'city'          => null,
+            'zipcode'       => null,
+            'cityDistrict'  => null,
+            'county'        => null,
+            'countyCode'    => null,
+            'region'        => null,
+            'regionCode'    => null,
+            'country'       => null,
+            'countryCode'   => null,
+            'timezone'      => null,
         );
     }
 
